ecfe65b9bfd225c92a838c709163d786208e2b2e
josepaolocea-alt <josepaolo.cea@gmail.com>
2026-08-20 16:28:34 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * Bump CACHE_VERSION to force old caches out on the next load.
  */
 
-const CACHE_VERSION = "promptvault-v6";
+const CACHE_VERSION = "promptvault-v7";
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
